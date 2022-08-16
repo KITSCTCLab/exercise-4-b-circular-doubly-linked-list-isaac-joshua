0612c25144ada0025aa0e1e739790eec42e6b4f6
isaac-joshua <110010536+isaac-joshua@users.noreply.github.com>
2022-08-16 12:33:08 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -11,5 +11,5 @@ while len(actual_list) < length_of_circular_linked_list and index < len(circular
         actual_list.append(element)
     index += 1
 
-print(len(actual_list))
-print(" ".join(str(num) for num in actual_list)),
+print(len())
+print(" ".join(str(num) for num in actual_list))
